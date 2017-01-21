9ffa7dbbb287195b4962a76f9f2176d13d8a149f
Jeremy <jeremyjnorwood@gmail.com>
2017-01-21 00:49:00 -0800

    Made Player 1 move with a controller

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject p3;
+
+    public bool p1_move;
+
+    float p1_horizontal;
+    float p1_vertical;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetAxis("Move1X") < -0.1f || Input.GetAxis("Move1X") > 0.1f || 
+            Input.GetAxis("Move1Y") < -0.1f || Input.GetAxis("Move1Y") > 0.1f)
+        {
+            p1_horizontal = Input.GetAxisRaw("Move1X");
+            p1_vertical = Input.GetAxisRaw("Move1Y");
+            p1_move = true;
+        }
+	}
+
+   void FixedUpdate()
+    {
+        if (p1_move == true)
+        {
+            p1.GetComponent<Transform>().position += new Vector3(p1_vertical, 0.0f, p1_horizontal) * 0.5f;
+            p1_move = false;
+        }
+    } 
+}
