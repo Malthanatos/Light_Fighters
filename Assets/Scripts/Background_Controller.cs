7688668c95df7dd1283d6b896d790940914ea530
Nathan Krueger <kruegern@uci.edu>
2017-01-21 21:22:25 -0800

    Perfected background, now only the scroll rate needs to be tinkered with s needed

--- Assets/Scripts/Background_Controller.cs
+++ Assets/Scripts/Background_Controller.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Background_Controller : MonoBehaviour {
 
     public float scrollx = 0.1f;
+    public float resetx = 2700.0f;
+    public float startx = -2650.0f;
 
 	void Start () {
-        transform.position = new Vector3(-2700.0f, -0.1f, 1.0f);
+        transform.position = new Vector3(startx, -0.1f, 1.0f);
 	}
 	
 	void FixedUpdate () {
-        if (transform.position.x >= 2700.0f)
+        if (transform.position.x >= resetx)
         {
-            transform.position = new Vector3(-2700.0f, -0.1f, 1.0f);
+            transform.position = new Vector3(startx, -0.1f, 1.0f);
         }
         transform.position += new Vector3(scrollx, 0.0f, 0.0f);
 	}
